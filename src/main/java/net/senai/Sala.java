f85b633417984377964011b596e77c2ae19e9605
bbatistadaniel <bbatistadaniel@outlook.com>
2024-08-12 11:39:45 -0300

    initial commit

---
+++ src/main/java/net/senai/Sala.java
@@ -0,0 +1,23 @@
+package net.senai;
+
+public class Sala {
+
+    private String nome;
+    private String materia;
+    private String periodo;
+    private String professor;
+
+    public Sala(String nome, String materia, String periodo, String professor){
+
+        this.nome = nome;
+        this.materia = materia;
+        this.periodo = periodo;
+        this.professor = professor;
+
+    }
+
+    public void listarAlunos(){
+
+    }
+
+}
